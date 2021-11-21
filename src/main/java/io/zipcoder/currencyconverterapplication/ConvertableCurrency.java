bc8e220a9f05348be61d70bce3c26276e9ee8f04
David Nguyen <davebox309@gmail.com>
2021-11-21 17:09:06 -0500

    completed currency type

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,16 @@
 package io.zipcoder.currencyconverterapplication;
 
 public interface ConvertableCurrency {
+
+
+    //default (keyword) allows us to apply this method to all of the implementing clasess
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        //current currency type, access get rate method / (currency rate) access .getCurrency method
+        // to return the rate
+        return currencyType.getRate() / this.getCurrencyType().getRate();
     }
+    //method stub to be applied to all the currency classes; see @Override
+    CurrencyType getCurrencyType();
+
+
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,16 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+
+
+
+    //is a method that is implemented thru the interface
+    @Override
+    public CurrencyType getCurrencyType() {
+        return CurrencyType.AUSTRALIAN_DOLLAR;
+    }
 }
